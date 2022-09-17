2621889a300b2d11b5f1773b9281fa98d7ab85ca
seetam-cto <seetam@creativejoule.in>
2022-09-18 00:50:55 +0530

    API Changes

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use(express.json())
 
 //route middlewares
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)))
-app.get('/', function (req, res) {
-    res.render('index', {});
-  });
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use('/', express.static(path.join(__dirname, '/frontend/build')))
 
 const port = process.env.PORT || 8000
 
