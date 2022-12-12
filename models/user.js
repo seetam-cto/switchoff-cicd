b293c0c8c96d25b5a4650be1f4a297a26bc812f8
seetam-cto <seetam@creativejoule.in>
2022-12-12 12:19:25 +0530

    fix: pass hash

--- models/user.js
+++ models/user.js
@@ -75,6 +75,23 @@ userSchema.pre('save', function(next) {
     
 })
 
+userSchema.pre('update', function(next) {
+    let user = this
+    if(user.isModified('password')){
+        return bcrypt.hash(user.password, 12, function(err, hash){
+            if(err){
+                console.log("BCRYPT HASH ERR: ", err)
+                return next(err)
+            }
+            user.password = hash
+            return next()
+        })
+    } else {
+        return next()
+    }
+    
+})
+
 userSchema.methods.comparePassword = function (password, next) {
     bcrypt.compare(password, this.password, function (err, match) {
         if(err){
