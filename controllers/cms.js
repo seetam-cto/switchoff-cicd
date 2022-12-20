63ffc3ab0ab63dd5650e30d0efd4ac20f30217c4
seetam-cto <seetam@creativejoule.in>
2022-12-20 15:46:43 +0530

    fix

--- controllers/cms.js
+++ controllers/cms.js
@@ -80,7 +80,7 @@ export const getSettings = async (req, res) => {
                 let general = await SettingGeneral.find().limit(1)
                 let homepage = await SettingHomepage.find().limit(1)
                 let footer = await SettingFooter.find().limit(1)
-                result = {general, homepage, footer}
+                result = {general: general[0], homepage: homepage[0], footer:  footer[0]}
                 if(!result) return res.status(400).send("Settings not found!")
                 break;
         }
